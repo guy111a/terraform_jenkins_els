9d37d36ffafbedd2368d755f17ae1dc595cde878
Your Name <avraham.guy@gmail.com>
2025-01-03 17:16:05 +0200

    sdf

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -30,12 +30,6 @@ module "vpc" {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
     "kubernetes.io/role/private-elb"       = 1
   }
-  data "aws_eks_cluster" "cluster" {
-    name = module.eks.cluster_id
-  }
-  data "aws_eks_cluster_auth" "cluster" {
-    name = module.eks.cluster_id
-  }
 }
 
 # cluster
