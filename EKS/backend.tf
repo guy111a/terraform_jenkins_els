9cddadf4375eb86e434acd0ba62dd1a8b915c208
Your Name <avraham.guy@gmail.com>
2025-01-04 20:57:57 +0200

    ff

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "buc111333555"
-    key    = "ec2-jenkins-server/terraform.tfstate"
-    region = "il-central-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "buc111333555"
+#     key    = "ec2-jenkins-server/terraform.tfstate"
+#     region = "il-central-1"
+#   }
+# }
