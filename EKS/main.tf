f9b7f00a367320014a32f6c79876f757e5214d09
Your Name <avraham.guy@gmail.com>
2025-01-04 23:40:57 +0200

    ff

--- EKS/main.tf
+++ EKS/main.tf
@@ -54,4 +54,14 @@ module "eks" {
     Environment = "dev"
     Terraform   = "true"
   }
+
+  data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+  }
+
+  data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+  }
+
+
 }
