2a2e5f3d1b7b2c113a14f2b58cf19c1a2c4a1b8b
Your Name <avraham.guy@gmail.com>
2025-01-04 22:11:26 +0200

    ff

--- EKS/main.tf
+++ EKS/main.tf
@@ -14,16 +14,16 @@ module "vpc" {
   single_nat_gateway   = true
 
   tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
     "kubernetes.io/role/elb"               = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
     "kubernetes.io/role/internal-elb"      = 1
   }
 
