24fbf61f086d7f8a2da77e88faeddb88d40dc397
Your Name <avraham.guy@gmail.com>
2025-01-06 11:13:56 +0200

    ff

---
+++ create_webapp_s3_bucket_aws/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name" {
+    type = string
+    description = "bucket name"
+    default = "mybucket-sdfsf433"    
+}
+
+variable "website_index_document" {
+    description = "website index"
+    default = "index.html"
+}
+
